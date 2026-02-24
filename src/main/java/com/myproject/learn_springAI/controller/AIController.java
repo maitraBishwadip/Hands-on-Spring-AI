dee64473f2110ef0caba384632fc720105d13a17
Bishwadip <bishwadipmaitra50@gmail.com>
2026-02-24 13:39:08 +0600

    Added promt engineering with system user assistant and user roles defined

--- src/main/java/com/myproject/learn_springAI/controller/AIController.java
+++ src/main/java/com/myproject/learn_springAI/controller/AIController.java
@@ -18,6 +18,18 @@ public class AIController {
     @GetMapping("/chat")
     public String getJoke(@RequestParam("message") String message) {
 
-        return chatclient.prompt(message).call().content();
+        return chatclient.prompt()
+                .system(
+                        """
+         You are an internal HR assistant . your role is to help the employees with
+         questions related to HR policies such as , leave policy . working hours , benifits, and code of conduct.
+         If a user asks for help with anything outside of these topics , kindly inform them that you can only assist with
+         queries related to HR policies.
+        """
+
+                )
+                .user(message)
+
+                .call().content();
     }
 }
