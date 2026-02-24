bda519387c3ccc6e2d00a60b4a441a24355738f5
Bishwadip <bishwadipmaitra50@gmail.com>
2026-02-24 17:18:05 +0600

    Prompt template uses:

--- src/main/java/com/myproject/learn_springAI/config/AIConfig.java
+++ src/main/java/com/myproject/learn_springAI/config/AIConfig.java
@@ -1,16 +1,29 @@
 package com.myproject.learn_springAI.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class AIConfig {
 
+    @Value("classpath:resources/prompttemplates/userpromt.st")
+    Resource systemMessage;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         // Use the builder passed in by Spring Boot's auto-configuration
-        return builder.build();
+        return builder
+                .defaultSystem(
+                        systemMessage.getFilename()
+
+                )
+                .defaultUser("""
+                        Hi , how can u help me ?
+                        """)
+                        .build();
     }
 }
 
--- src/main/java/com/myproject/learn_springAI/controller/AIController.java
+++ src/main/java/com/myproject/learn_springAI/controller/AIController.java
@@ -4,6 +4,8 @@ package com.myproject.learn_springAI.controller;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,20 +17,17 @@ public class AIController {
 
     private final ChatClient chatclient;
 
+
+
     @GetMapping("/chat")
     public String getJoke(@RequestParam("message") String message) {
 
         return chatclient.prompt()
-                .system(
-                        """
-         You are an internal HR assistant . your role is to help the employees with
-         questions related to HR policies such as , leave policy . working hours , benifits, and code of conduct.
-         If a user asks for help with anything outside of these topics , kindly inform them that you can only assist with
-         queries related to HR policies.
-        """
 
-                )
-                .user(message)
+        .user(message)
+
+
+
 
                 .call().content();
     }
